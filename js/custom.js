5af74c930902e078543c8c2cdd832e2c973937b9
SIDATE El Mahdi <thehunckerma@hotmail.com>
2020-09-15 02:03:43 +0100

    Update light and dark modes colors

--- js/custom.js
+++ js/custom.js
@@ -12,19 +12,51 @@
   headroom.init();
 
   /* =================== THEME =================== */
-  var light = true;
+  var _light = true;
+
+  function theme() {
+    var theme = localStorage.getItem("theme");
+
+    if (theme === "dark") {
+      _light = false;
+      dark();
+    } else if (theme === "light") {
+      _light = true;
+      light();
+    } else {
+      localStorage.setItem("theme", "light"); // Set default theme
+      _light = true;
+      light();
+    }
+  }
+
+  function dark() {
+    _("body").classList.add("dark-mode");
+    _("#sun").style.display = "inline";
+    _("#moon").style.display = "none";
+    _("#svg-about").src = "/images/undraw/svg_dark.svg";
+    localStorage.setItem("theme", "dark");
+  }
+
+  function light() {
+    _("body").classList.remove("dark-mode");
+    _("#sun").style.display = "none";
+    _("#moon").style.display = "inline";
+    _("#svg-about").src = "/images/undraw/svg_light.svg";
+    localStorage.setItem("theme", "light");
+  }
+
+  theme();
+
+  document.defaultView.addEventListener("storage", theme); // LocalStorage event listener
 
   _("#theme-button").addEventListener("click", function () {
-    if (light) {
-      _("body").classList.add("dark-mode");
-      _("#sun").style.display = "inline";
-      _("#moon").style.display = "none";
-      light = false;
+    if (_light) {
+      dark();
+      _light = false;
     } else {
-      _("body").classList.remove("dark-mode");
-      _("#sun").style.display = "none";
-      _("#moon").style.display = "inline";
-      light = true;
+      light();
+      _light = true;
     }
   });
 
